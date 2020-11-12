5704a28fddd36441ef884b49cd5a7762ac626125
unknown <allinharmon@gmail.com>
2020-11-12 17:33:46 -0500

    Added more complete answer to bonus question

--- homework2/os.js
+++ homework2/os.js
@@ -21,15 +21,34 @@ console.log(os.uptime());
 //   BONUS: Write your computer specs to a file called specs.txt Check out the attached image and mimic a similar format. HINT: You'll want the path & fs module for this
 
 //add Bonus Question===============================================
-fs.writeFile(
-    'C://Users/Allin/Desktop/CBC10/week8/homework2/specs.txt',
-    //add math later for total memory, and uptime
+
+// //? Using .writeFile to only write it once to the file
+// fs.writeFile(path.join(__dirname, 'specs.txt'),
+
+//     `
+// Number of CPUs:     ${os.cpus().length}
+// Platform:           ${os.platform()}
+// Architecture:       ${os.arch()}
+// Total Memory:       ${os.totalmem()/1071884800} GBs
+// Uptime:             ${Math.floor((os.uptime()/60)/60)} to ${Math.ceil((os.uptime()/60)/60)} hours
+// `,
+//     {}, 
+//     (err) => {
+//         if(err) {
+//             console.log(err);
+//         }
+//     }
+// );
+
+//? Using .appendFile to write multiple times to the same file
+fs.appendFile(path.join(__dirname, 'specs.txt'),
+
     `
 Number of CPUs:     ${os.cpus().length}
 Platform:           ${os.platform()}
 Architecture:       ${os.arch()}
-Total Memory:       ${os.totalmem()}
-Uptime:             ${os.uptime()}
+Total Memory:       ${os.totalmem()/1071884800} GBs
+Uptime:             ${Math.floor((os.uptime()/60)/60)} to ${Math.ceil((os.uptime()/60)/60)} hours
 `,
     {}, 
     (err) => {
